66f624db519dd505dd26e3bfd83fde5a11e83a87
marieleklering <33419148+marieleklering@users.noreply.github.com>
2017-11-07 11:40:47 -0200

    Create oracle_install.sh

---
+++ oracle_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/oracle/midias/database
+./runInstaller -silent -responseFile /home/oracle/midias/db_install.rsp -ignoreSysPrereqs -ignorePrereq -waitforcompletion -showProgress
+./u01/app/oraInventory/orainstRoot.sh
+./u01/app/oracle/product/11.2.0/db_1/root.sh
+
+##config netca
+cd /home/oracle/midias
+netca -silent -responseFile /home/oracle/midias/netca.rsp
+
+##install DB
+dbca -silent -responseFile /home/oracle/midias/dbca.rsp
