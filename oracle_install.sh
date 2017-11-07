9b97c7015eecc040ec5521cbc4d13c014be6fc6d
marieleklering <33419148+marieleklering@users.noreply.github.com>
2017-11-07 16:17:23 -0200

    Update oracle_install.sh

--- oracle_install.sh
+++ oracle_install.sh
@@ -5,6 +5,7 @@ sudo -u oracle ./runInstaller -silent -responseFile /home/oracle/midias/db_insta
 ./u01/app/oracle/product/11.2.0/db_1/root.sh
 
 ##config netca
+/home/oracle/db.env 
 cd /home/oracle/midias
 sudo -u oracle netca -silent -responseFile /home/oracle/midias/netca.rsp
 
