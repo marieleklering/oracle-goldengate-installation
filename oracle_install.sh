c2143f0edf54a5bcfac318db5661a1df47e207fc
marieleklering <33419148+marieleklering@users.noreply.github.com>
2017-11-07 17:22:56 -0200

    Update oracle_install.sh

--- oracle_install.sh
+++ oracle_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/oracle/midias/database
-sudo -u oracle ./runInstaller -silent -responseFile /home/oracle/midias/db_install.rsp -ignoreSysPrereqs -ignorePrereq -waitforcompletion -showProgress
+su - oracle -c "./runInstaller -silent -responseFile /home/oracle/midias/db_install.rsp -ignoreSysPrereqs -ignorePrereq -waitforcompletion -showProgress"
 
 /u01/app/oraInventory/orainstRoot.sh
 /u01/app/oracle/product/11.2.0/db_1/root.sh
